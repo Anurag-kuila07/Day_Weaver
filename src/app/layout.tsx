1baa10f770b423acc73a358dac985080730a5741
Anurag Kuila <anuragkuila5@gmail.com>
2025-09-28 08:44:44 +0000

    Copy and paste the AdSense code in between the <head></head> tags of you

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4407667957128270"
+     crossOrigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
