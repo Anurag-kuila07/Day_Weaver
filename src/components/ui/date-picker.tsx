5c9ced275d024684b0c27aef4813cfec39002e9c
Anurag Kuila <anuragkuila5@gmail.com>
2025-09-30 05:18:43 +0000

    when user click on a perticular date calender should be hide

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -19,9 +19,10 @@ type DatePickerProps = {
 }
 
 export function DatePicker({ date, setDate }: DatePickerProps) {
+  const [open, setOpen] = React.useState(false);
 
   return (
-    <Popover>
+    <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
           variant={"outline"}
@@ -38,7 +39,10 @@ export function DatePicker({ date, setDate }: DatePickerProps) {
         <Calendar
           mode="single"
           selected={date}
-          onSelect={setDate}
+          onSelect={(selectedDate) => {
+            setDate(selectedDate);
+            setOpen(false);
+          }}
           initialFocus
         />
       </PopoverContent>
